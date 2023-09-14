877b254d967e3a21f4b2a80f488239a28e2b46bb
clemcheyrou <clemcheyrou@gmail.com>
2023-09-14 16:09:21 +0200

    WIP

--- srcs/requirements/wordpress/conf/wp-setup.sh
+++ srcs/requirements/wordpress/conf/wp-setup.sh
@@ -4,7 +4,7 @@ sleep 10
 
 if ! wp core is-installed --allow-root  ; then
 	wp core download --allow-root --force
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER--dbpass=$MYSQL_PASSWORD --dbhost="mariadb:3306" --path='/var/www/wordpress'--allow-root --force
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER--dbpass=$MYSQL_PASSWORD --dbhost=localhost --path='/var/www/wordpress'--allow-root --force
 	wp core install --url="ccheyrou.42.fr" --title="Inception" --admin_user=$WORDPRESS_DB_ADMIN_USER --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email="ccheyrou@student.42.fr" --allow-root
 	wp user create $WORDPRESS_DB_USER ccheyrou@student.42.fr --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root
 	wp config shuffle-salts --allow-root
