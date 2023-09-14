23ab703d68c846a3f5394f5ae995999962b7e9cc
clemcheyrou <clemcheyrou@gmail.com>
2023-09-14 15:01:18 +0200

    WIP

--- srcs/requirements/mariadb/conf/mariadb.sh
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -7,7 +7,7 @@ echo "FLUSH PRIVILEGES;" >> file
 echo "GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> file
 echo "FLUSH PRIVILEGES;" >> file
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> file
-echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"  >> file
+echo "GRANT ALL ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"  >> file
 echo "FLUSH PRIVILEGES;" >> file
 
 mysqld --user=mysql --verbose --bootstrap < file
